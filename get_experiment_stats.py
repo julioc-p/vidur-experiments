fed324511c5e5f6e0d91af0402b880e7de69ee91
Prow CI Robot <dev@kubermatic.com>
2025-01-13 15:14:06 +0100

    python script to gather all stats for each configuration
    Signed-off-by: Prow CI Robot <dev@kubermatic.com>

---
+++ get_experiment_stats.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import argparse
+import os
+import json
+
+def get_experiments_stats(experiment_dir):
+  results = {}
+  for folder in os.listdir(experiment_dir):
+    if not folder.startswith("replicas"):
+      continue
+    avg_request_scheduling_delay, mfu = get_folder_stats(experiment_dir, folder)
+    results[folder] = {"avg_request_scheduling_delay": avg_request_scheduling_delay, "avg_mfu": mfu}
+  return results
+
+def get_folder_stats(experiment_dir, folder):
+  num_requests = 0
+  total_request_scheduling_delay = 0
+  total_mfu = 0
+  replicas = 0
+  experiments_dir = os.path.join(experiment_dir, folder)
+  for folder in os.listdir(experiments_dir):
+    num_requests_single_experiment, total_request_scheduling_delay_single_experiment, total_mfu_single_experiment, replicas_single_experiment = get_single_experiment_stats(os.path.join(experiments_dir, folder))
+    num_requests += num_requests_single_experiment
+    total_request_scheduling_delay += total_request_scheduling_delay_single_experiment
+    total_mfu += total_mfu_single_experiment
+    replicas += replicas_single_experiment
+  
+
+  try:
+    avg_request_scheduling_delay = total_request_scheduling_delay / num_requests
+  except ZeroDivisionError:
+    avg_request_scheduling_delay = 0
+
+  try:
+    mfu = total_mfu / replicas
+  except ZeroDivisionError:
+    mfu = 0
+
+  return avg_request_scheduling_delay, mfu
+
+def get_single_experiment_stats(experiment_dir):
+  try:
+    request_metrics = pd.read_csv(os.path.join(experiment_dir, "request_metrics.csv"))
+    num_requests = request_metrics.shape[0]
+    total_request_scheduling_delay = request_metrics["request_scheduling_delay"].sum()
+    num_replicas = get_number_replicas(experiment_dir)
+    total_mfu = get_mfu(experiment_dir)
+    return num_requests, total_request_scheduling_delay, total_mfu, num_replicas
+  except FileNotFoundError as e:
+    return 0, 0, 0, 0
+
+def get_number_replicas(experiment_dir):
+  replicas = 0
+  # for each file in the plots folder
+  for file in os.listdir(os.path.join(experiment_dir, "plots")):
+    if "mfu" in file:
+      replicas += 1
+  return replicas
+  
+def get_mfu(experiment_dir):
+  mfu = 0
+  for file in os.listdir(os.path.join(experiment_dir, "plots")):
+    if "mfu" in file:
+      mfu += get_mfu_single_replica(os.path.join(experiment_dir, "plots", file))
+  return mfu
+
+def get_mfu_single_replica(file):
+  mfu = 0
+  with open(file) as f:
+    data = json.load(f)
+    for key in data:
+      if key.endswith("weighted_mean"):
+        mfu += data[key]
+  return mfu
+
+
+if __name__ == '__main__':
+
+    experiment_dir = "simulator_output"
+    experiment_stats = get_experiments_stats(experiment_dir)
+    print(experiment_stats)
--- run_experiment.sh
+++ run_experiment.sh
@@ -2,7 +2,7 @@
 
 # Default values for parameters
 NUM_REPLICAS=1
-QPS=1
+QPS=0.5
 BATCH_SIZE=1
 
 # Parse command-line arguments
@@ -27,6 +27,8 @@ while [[ "$#" -gt 0 ]]; do
   esac
 done
 
+OUTPUT_DIR="simulator_output/replicas_${NUM_REPLICAS}_batch_${BATCH_SIZE}_qps_${QPS}"
+
 # Run the Python command with the specified parameters
 python -m vidur.main \
   --replica_config_device a100 \
@@ -35,14 +37,15 @@ python -m vidur.main \
   --replica_config_tensor_parallel_size 1 \
   --replica_config_num_pipeline_stages 1 \
   --request_generator_config_type synthetic \
-  --synthetic_request_generator_config_num_requests 512 \
+  --synthetic_request_generator_config_num_requests 10000 \
   --length_generator_config_type trace \
-  --trace_request_length_generator_config_max_tokens 16384 \
+  --trace_request_length_generator_config_max_tokens 4096 \
   --trace_request_length_generator_config_trace_file ./data/processed_traces/splitwise_conv.csv \
   --interval_generator_config_type poisson \
   --poisson_request_interval_generator_config_qps $QPS \
   --replica_scheduler_config_type vllm \
   --vllm_scheduler_config_batch_size_cap $BATCH_SIZE \
-  --random_forrest_execution_time_predictor_config_prediction_max_prefill_chunk_size 16384 \
+  --random_forrest_execution_time_predictor_config_prediction_max_prefill_chunk_size 4096 \
   --random_forrest_execution_time_predictor_config_prediction_max_batch_size $BATCH_SIZE \
-  --random_forrest_execution_time_predictor_config_prediction_max_tokens_per_request 16384
+  --random_forrest_execution_time_predictor_config_prediction_max_tokens_per_request 4096 \
+  --metrics_config_output_dir $OUTPUT_DIR
